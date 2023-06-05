b2c83eee48403c8975ade2343d4201e66f5c38a3
lmx1011 <2250592836@qq.com>
2023-06-05 23:21:23 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import './App.css'
-
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      React Template
-    </div>
-  );
+  return <div className="App">React Template</div>;
 }
 
 export default App;
